caa6539f186bf37200f10e80636cf94ccc895382
ethanfel <ethan.fel@ts-pc.fr>
2021-07-01 18:38:30 +0200

    Update flipflop.sh
    A simple data augmentation script. (x4) file/flip/flop/flipflop
    
    Be carefull and don't use twice.

--- make_datasets/flipflop.sh
+++ make_datasets/flipflop.sh
@@ -5,13 +5,14 @@ cd "${1}"
 
 for file in *.jpg; do
     convert "${file}" -flip "flip_${file}";
-    convert "${file}" -flop "flop_${file}";
+done &&
+for file in *.jpg; do
+    convert "${file}" -flop "flop_${file}"; 
 done
 
 for file in *.png; do
     convert "${file}" -flip "flip_${file}";
+done  &&
+for file in *.png; do
     convert "${file}" -flop "flop_${file}";
 done
-
-
-
